83ab1878a0ac32b87fc306c1598e58c67be8209f
Ben L. Titzer <ben.titzer@gmail.com>
2024-02-03 13:37:20 -0500

    [test] Update test to run in multiple modes

--- Makefile
+++ Makefile
@@ -3,12 +3,20 @@
 all: bin/progress.host
 
 bin/progress.host: src/progress.c
+	echo "##+build bin/progress.host"
 	cc -o bin/progress.host -Ibin src/progress.c
-	bash -x bin/.auto-progress < test/make.in
+	echo "##-ok"
 
-.PHONY: clean
+.PHONY: clean test
 clean:
 	rm -f bin/progress
 	rm -f bin/progress.host
 	cp bin/.auto-progress bin/progress
 
+current_dir = $(shell pwd)
+export PROGRESS := ${current_dir}/bin/progress.host
+
+test: bin/progress.host
+	bash -x bin/.auto-progress s < test/make.in
+	echo ${PROGRESS}
+	bash test/run.bash
--- test/run.bash
+++ test/run.bash
@@ -8,6 +8,16 @@ while [ -h "$SOURCE" ]; do
 done
 DIR="$( cd -P "$( dirname "$SOURCE" )" >/dev/null 2>&1 && pwd )"
 
+PROGRESS=${PROGRESS:=$DIR/../bin/progress.host}
+
+echo "##+check PROGRESS=$PROGRESS"
+if [ -x $PROGRESS ]; then
+    echo "##-ok"
+else
+    echo "##-fail: not executable"
+    exit 1
+fi
+
 cd $DIR
 if [ $# -gt 0 ]; then
     TESTS=$*
@@ -19,17 +29,40 @@ T=/tmp/$USER/progress-test/
 
 mkdir -p $T
 
-PROGRESS=${PROGRESS:=$DIR/../bin/progress.host}
+DEBUG=${DEBUG:=0}
+
+function debug() {
+    if [ $DEBUG != 0 ]; then
+	echo "$@"
+    fi
+}
 
 for f in $TESTS; do
-    echo "##+$f"
-    OUT=$T/$(basename $f)
-    OUT=${OUT/in/out}
-    $PROGRESS < $f > $OUT
-    diff ${f/in/out} $OUT > $OUT.diff
-    if [ "$?" = 0 ]; then
-	echo "##-ok"
-    else
-	echo "##-fail: $OUT.diff"
-    fi
+    test=$(basename $f)
+    test=${test/.in/}
+    base=${f/.in/}
+    debug "==> $f"
+
+    regex=".*-(.*).out"
+    for expfile in ${base}*.out; do
+	debug "  ==> expf: $expfile"
+	args=""
+	if [[ $expfile =~ $regex ]]; then
+	    args="${BASH_REMATCH[1]}"
+	fi
+	debug "  ==> args: $args"
+
+	
+	echo "##+progress $args < $f"
+	OUT=$T/$B.$args.out
+	DIFF=$T/$B.$args.diff
+	$PROGRESS $args < $f > $OUT
+	diff $expfile $OUT > $DIFF
+	if [ "$?" = 0 ]; then
+	    echo "##-ok"
+	else
+	    echo "##-fail: $DIFF"
+	fi
+	
+    done
 done
